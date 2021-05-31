057d064947531486fdacc75ec297be440f3a9d55
yanjiaxuan <330544968@qq.com>
2021-06-01 02:16:11 +0800

    fix history order problem

--- src/renderer/views/history/history.tsx
+++ src/renderer/views/history/history.tsx
@@ -5,6 +5,14 @@ import TomatoxWaterfall from '@/components/tomatox-waterfall/tomatox-waterfall';
 import Scrollbars from 'react-custom-scrollbars';
 import cssM from './history.scss';
 
+function compareYMStr(a: string, b: string): number {
+    return +a.replace('年', '').replace('月', '') - +b.replace('年', '').replace('月', '');
+}
+
+function compareDStr(a: string, b: string): number {
+    return parseInt(b, 10) - parseInt(a, 10);
+}
+
 export default class History extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
@@ -16,33 +24,52 @@ export default class History extends React.Component<any, any> {
     async componentWillMount() {
         const res = await Indexed.instance!.queryAll(TABLES.TABLE_HISTORY);
         const resources = res as IplayResource[];
-        const resCovertRes = new Map<string, Map<string, IplayResource[]>>();
-        resources
-            .sort((a, b) => a.historyOption!.lastPlayDate! - b.historyOption!.lastPlayDate!)
-            .forEach(resource => {
-                const date = new Date(resource.historyOption!.lastPlayDate!);
-                const yearMonth = `${date.getFullYear()}年${date.getMonth() + 1}月`;
-                const day = `${date.getDate()}日`;
-                if (!resCovertRes.get(yearMonth)) {
-                    resCovertRes.set(yearMonth, new Map<string, IplayResource[]>());
-                }
-                if (!resCovertRes.get(yearMonth)!.get(day)) {
-                    resCovertRes.get(yearMonth)!.set(day, new Array<IplayResource>());
-                }
-                resCovertRes
-                    .get(yearMonth)!
-                    .get(day)!
-                    .push(resource);
+        const resMap = new Map<string, Map<string, IplayResource[]>>();
+        // step1: convert list to map
+        resources.forEach(resource => {
+            const date = new Date(resource.historyOption!.lastPlayDate!);
+            const yearMonth = `${date.getFullYear()}年${date.getMonth() + 1}月`;
+            const day = `${date.getDate()}日`;
+            if (!resMap.get(yearMonth)) {
+                resMap.set(yearMonth, new Map<string, IplayResource[]>());
+            }
+            if (!resMap.get(yearMonth)!.get(day)) {
+                resMap.get(yearMonth)!.set(day, new Array<IplayResource>());
+            }
+            resMap
+                .get(yearMonth)!
+                .get(day)!
+                .push(resource);
+        });
+        // step2: sort
+        const ymSortedArr = Array.from(resMap.keys()).sort(compareYMStr);
+        const sortedMap = new Map<string, Map<string, IplayResource[]>>();
+        ymSortedArr.forEach((ym: string) => {
+            const dayMap = resMap.get(ym)!;
+            const daySortedMap = new Map<string, IplayResource[]>();
+            const daySortedArr = Array.from(dayMap.keys()).sort(compareDStr);
+            daySortedArr.forEach(day => {
+                daySortedMap.set(
+                    day,
+                    dayMap
+                        .get(day)!
+                        .sort(
+                            (a, b) =>
+                                b.historyOption!.lastPlayDate! - a.historyOption!.lastPlayDate!
+                        )
+                );
             });
+            sortedMap.set(ym, daySortedMap);
+        });
         this.setState({
-            resourceList: resCovertRes
+            resourceList: sortedMap
         });
     }
 
     renderD(dData: Map<string, IplayResource[]>, ym: string) {
         const res: ReactElement[] = [];
         dData.forEach((value, key) => {
-            res.unshift(
+            res.push(
                 <div key={key}>
                     <div className={cssM.yearMonthStyle}>{ym}</div>
                     <div className={cssM.dayStyle}>{key}</div>
