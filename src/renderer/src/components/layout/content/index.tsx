662cf364a765eb3b537d1e64233b85811eb1dbba
yanjiaxuan <330544968@qq.com>
2022-12-17 18:35:30 +0800

    open keepalive and give up route transition

--- src/renderer/src/components/layout/content/index.tsx
+++ src/renderer/src/components/layout/content/index.tsx
@@ -1,22 +1,15 @@
 import { useState } from 'react'
-
 import { Spin } from 'antd'
-import { Outlet, useLocation } from 'react-router-dom'
-import { SwitchTransition, CSSTransition } from 'react-transition-group'
+import { Outlet } from 'react-router-dom'
 import './index.less'
 
 export default function TomatoxContent(): JSX.Element {
   const [loading] = useState(false)
-  const location = useLocation()
 
   return (
     <Spin size={'large'} spinning={loading} tip={'Loading...'} style={{ maxHeight: 'unset' }}>
       <div className={'content-wrapper'}>
-        <SwitchTransition mode={'out-in'}>
-          <CSSTransition key={location.key} timeout={300} classNames="fade" nodeRef={null}>
-            <Outlet />
-          </CSSTransition>
-        </SwitchTransition>
+        <Outlet />
       </div>
     </Spin>
   )
