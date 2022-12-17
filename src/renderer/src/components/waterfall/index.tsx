b3d0f341547e3d1012a5f811db9a2fa8266e1c23
yanjiaxuan <330544968@qq.com>
2022-12-18 00:53:51 +0800

    remove infinite scroll

--- src/renderer/src/components/waterfall/index.tsx
+++ src/renderer/src/components/waterfall/index.tsx
@@ -1,29 +1,26 @@
-import { ReactNode } from 'react'
 import { HeartOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import './index.less'
 import { Card, Image } from 'antd'
 
 export default function Waterfall(props: { data: IPlayResource[] }): JSX.Element {
-  const cardsData = props.data
-  function convertEle(): ReactNode {
-    const result: ReactNode[] = []
-    for (const ele of cardsData) {
-      result.push(
-        <Link to={'/play'}>
+  return (
+    <div className={'card-list'}>
+      {props.data.map((item) => (
+        <Link to={'/play'} key={item.id}>
           <Card
-            key={ele.id}
-            cover={<Image src={ele.picture} className={'card-cover'} preview={false} />}
+            cover={<Image src={item.picture} className={'card-cover'} preview={false} />}
             className={'card'}
             hoverable={true}
-            actions={[<span key={'remark'}>{ele.remark}</span>, <HeartOutlined key={'ellipsis'} />]}
+            actions={[
+              <span key={'remark'}>{item.remark}</span>,
+              <HeartOutlined key={'ellipsis'} />
+            ]}
           >
-            <Card.Meta title={ele.name} description={ele.actor || '未知'} />
+            <Card.Meta title={item.name} description={item.actor || '未知'} />
           </Card>
         </Link>
-      )
-    }
-    return result
-  }
-  return <div className={'card-list'}>{convertEle()}</div>
+      ))}
+    </div>
+  )
 }
